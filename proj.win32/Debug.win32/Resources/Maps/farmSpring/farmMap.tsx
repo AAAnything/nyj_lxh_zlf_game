20d07752778fa226cba7c7a7c1554386f060e84f
emma <1964357322@qq.com>
2025-12-23 21:23:24 +0800

    更新修改了farmMap.png和farmMap.tmx，添加farmMap.tsx外部文件；增加了树和草的元素，重做了瓦片地图（.tmx）;

---
+++ proj.win32/Debug.win32/Resources/Maps/farmSpring/farmMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="farmMap" tilewidth="64" tileheight="64" tilecount="7396" columns="86">
+ <image source="farmMap.png" width="5504" height="5504"/>
+</tileset>
